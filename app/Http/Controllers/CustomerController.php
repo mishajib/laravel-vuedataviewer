a33b9e1b80371dbb3143688f5dd889a98b0dabce
mishajib <mishajib222@gmail.com>
2020-09-07 21:05:09 +0600

    initial commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function __invoke(Request $request)
+    {
+        return view('customers');
+    }
+}
